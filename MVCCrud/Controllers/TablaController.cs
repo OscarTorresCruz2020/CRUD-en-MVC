4f4657e69394616bd9202906dc7c79cfdb4cef2b
OscarTorresCruz2020 <oscarintorres2020@hotmail.com>
2021-08-27 19:20:39 -0500

    version 1

---
+++ MVCCrud/Controllers/TablaController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCCrud.Models;
+using MVCCrud.Models.ViewModels;
+
+namespace MVCCrud.Controllers
+{
+    public class TablaController : Controller
+    {
+        // GET: Tabla
+        public ActionResult Index()
+        {
+            List<ListTablaViewModel> lst;
+            using (CrudEntities db= new CrudEntities())
+            {
+                lst= (from d in db.Tabla
+                         select new ListTablaViewModel
+                         {
+                             Id=d.id,
+                             Nombre=d.nombre,
+                             Correo =d.correo
+                             
+                             
+                         }).ToList();
+            }
+            return View(lst);
+        }
+
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Nuevo(TablaViewModel model)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    using (CrudEntities db= new CrudEntities())
+                    {
+                        var oTabla = new Tabla();
+                        oTabla.correo = model.Correo;
+                        oTabla.fecha_nacimiento = model.Fecha_Nacimiento;
+                        oTabla.nombre = model.Nombre;
+
+                        db.Tabla.Add(oTabla);
+                        db.SaveChanges();
+                    }
+
+                    return Redirect("~/Tabla/");
+                }
+
+                return View(model);
+                
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+
+            }
+
+        }
+
+        public ActionResult Editar(int Id)
+        {
+            TablaViewModel model = new TablaViewModel();
+            using (CrudEntities db = new CrudEntities())
+            {
+                var oTabla = db.Tabla.Find(Id);
+                model.Nombre = oTabla.nombre;
+                model.Correo = oTabla.correo;
+                model.Id = oTabla.id;
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(TablaViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (CrudEntities db = new CrudEntities())
+                    {
+                        var oTabla = db.Tabla.Find(model.Id);
+                        oTabla.correo = model.Correo;
+                        oTabla.fecha_nacimiento = model.Fecha_Nacimiento;
+                        oTabla.nombre = model.Nombre;
+
+                        db.Entry(oTabla).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                    }
+
+                    return Redirect("~/Tabla/");
+                }
+
+                return View(model);
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+
+            }
+
+        }
+
+        [HttpGet]
+        public ActionResult Eliminar(int Id)
+        {
+            TablaViewModel model = new TablaViewModel();
+            using (CrudEntities db = new CrudEntities())
+            {
+                var oTabla = db.Tabla.Find(Id);
+                db.Tabla.Remove(oTabla);
+                db.SaveChanges();
+            }
+            return Redirect("~/Tabla/");
+        }
+    }
+}
